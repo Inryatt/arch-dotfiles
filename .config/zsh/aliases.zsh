1ff8efa32a81ffdcd9033da86521462e661940ef
Inryatt <inrymail@gmail.com>
2024-05-16 19:12:46 +0100

    Initial backup

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,13 @@
+
+
+## ALIASES
+alias grep="grep --color=always"
+alias emulator="QT_QPA_PLATFORM=xcb emulator"
+alias obs="QT_QPA_PLATFORM=xcb obs"
+alias ls="lsd -A1l"
+alias cat="bat"
+alias x="exit"
+alias vim="nvim"
+alias p3="python3"
+alias cfg='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
